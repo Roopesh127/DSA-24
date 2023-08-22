69480a801c480a857b5d0244e07ba6203dd29da3
Roopesh127 <roopeshvish2006@gmmail.com>
2023-08-22 13:01:43 +0530

    added 2

---
+++ DAY-2/2.js
@@ -0,0 +1,10 @@
+
+// String reverse without using inbult function
+
+var x =  "satyam";
+ var reversearray = [];
+for(let i = x.length-1;i>=0;i--){
+    var z= x[i]
+    reversearray.push(z);
+    }
+console.log(reversearray);
