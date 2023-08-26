9eaf349217bc7adf678a4c44dd41a1c0b4689427
Roopesh127 <roopeshvish2006@gmmail.com>
2023-08-26 15:41:48 +0530

    newww

---
+++ DAY-2/5.js
@@ -0,0 +1,11 @@
+//-> Swapping of 2 numbers with third variable 
+
+// function swapping(a=5,b=10){
+// let c = a;
+// a = b;
+// b= c;
+// console.log(`value of a = ${a}, value of b = ${b}`)
+// }
+// swapping();
+
+
