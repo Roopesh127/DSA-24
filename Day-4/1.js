f529a58dabeae02b07996be1c1f2022290c7c0ce
Roopesh127 <roopeshvish2006@gmmail.com>
2023-10-06 15:06:45 +0530

    hello

--- Day-4/1.js
+++ Day-4/1.js
@@ -1,2 +1,19 @@
 // Question - To find longest word from a string using custom code
-const Word = " Hello guys how are you";
+// const w = " Helllo guys how are you";
+
+function longestword(w) {
+    let splited = w.split(' ')
+    let longest = "";
+    for (let i = 0; i < splited.length; i++) {
+
+        if (splited[i].length > longest.length) {
+            // console.log(splited[i], longest)
+
+            longest = splited[i];
+        }
+    }
+    // console.log({ longest })
+    return longest;
+    // console.log(splited)
+}
+console.log(longestword("Helllo guyuus how are you"))
