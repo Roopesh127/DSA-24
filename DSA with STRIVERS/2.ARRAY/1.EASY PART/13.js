55834e389ab761c27086f432d3eda02cf0d1bd5c
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2023-11-03 23:30:56 +0530

    hello new

---
+++ DSA with STRIVERS/2.ARRAY/1.EASY PART/13.js
@@ -0,0 +1,24 @@
+
+function getLongestSubarray(a, k) {
+    let n = a.length; // size of the array
+
+    let len = 0;
+    for (let i = 0; i < n; i++) { // starting index
+        for (let j = i; j < n; j++) { // ending index
+            // add all the elements of subarray = a[i...j]
+            let s = 0;
+            for (let K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s === k)
+                len = Math.max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+let a = [2, 3, 5, 1, 9];
+let k = 10;
+let len = getLongestSubarray(a, k);
+console.log("The length of the longest subarray is:", len);
