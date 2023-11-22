d0d7909e80bf035cbba276e6c1db8c8a898af814
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2023-11-22 23:31:26 +0530

    n

---
+++ DSA with STRIVERS/2.ARRAY/2. MEDIUM PART/1.Two Sum - Check if a pair with given sum exists in Array.js
@@ -0,0 +1,15 @@
+// Check if a pair with given sum exists in Array
+
+var twoSum = function(nums, target) {
+    let mp = new Map()
+    
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i]
+        
+        if (mp.has(diff)) {
+            return [i, mp.get(diff)]
+        }
+        
+        mp.set(nums[i], i)
+    }
+};
