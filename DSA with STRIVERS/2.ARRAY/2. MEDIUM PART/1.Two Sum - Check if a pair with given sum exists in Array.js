06fa71b26d3adb04a3f54a6203f887f4ce744245
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2023-11-23 00:08:50 +0530

    new

--- DSA with STRIVERS/2.ARRAY/2. MEDIUM PART/1.Two Sum - Check if a pair with given sum exists in Array.js
+++ DSA with STRIVERS/2.ARRAY/2. MEDIUM PART/1.Two Sum - Check if a pair with given sum exists in Array.js
@@ -1,15 +1,33 @@
 // Check if a pair with given sum exists in Array
+// Example 1:
 
-var twoSum = function(nums, target) {
-    let mp = new Map()
-    
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
-        
-        if (mp.has(diff)) {
-            return [i, mp.get(diff)]
-        }
-        
-        mp.set(nums[i], i)
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+function findSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == target) {
+        return [i, j];
+      }
+      
     }
-};
+}
+    return null;
+}
+console.log(findSum([3,0,0,3,2,4], 5))
+
+// var twoSum = function(nums, target) {
+//     let mp = new Map()
+
+//     for (let i = 0; i < nums.length; i++) {
+//         let diff = target - nums[i]
+
+//         if (mp.has(diff)) {
+//             return [i, mp.get(diff)]
+//         }
+
+//         mp.set(nums[i], i)
+//     }
+// };
