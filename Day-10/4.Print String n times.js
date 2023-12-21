51c3ce2d1af1b82924aaae777df18b949c716ad0
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2023-12-21 09:16:55 +0530

    new

---
+++ Day-10/4.Print String n times.js
@@ -0,0 +1,16 @@
+// Print string n times using inbuilt function
+
+// ----> 1. By repeat method
+function RepeatString(str,n){
+    console.log(str.repeat(n))
+}
+RepeatString("Roopesh", 6 )
+
+
+// ----> 2. By Simple for loop
+// function PrintString(n,str){
+//      for(let i=0;i<n;i++){
+//         console.log(str)
+//      }
+// }
+// PrintString(10,"Roopesh")
