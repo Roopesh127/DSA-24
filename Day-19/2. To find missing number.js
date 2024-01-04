89afde4b59d7f9fffc3e75727deca66095158745
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-01-04 12:11:09 +0530

    new

--- Day-19/2. To find missing number.js
+++ Day-19/2. To find missing number.js
@@ -2,3 +2,18 @@
 // const input = [1, 2, 6];
 
 // Output = [3, 4, 5];
+
+function MissingNumber(arr){
+    let blank = [];
+   for(let i=0;i<arr.length;i++){
+      // console.log(arr[i])
+     for(let j = i+1;j<=arr.length;j++){
+      //   if((arr[j] - arr[i]) > 1){
+      //       blank.push(arr[i]+2);
+      //   }
+     }
+   }
+   // console.log(blank)
+}
+MissingNumber([1, 2, 6]);
+
