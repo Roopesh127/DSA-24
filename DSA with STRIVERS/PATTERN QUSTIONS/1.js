ea06ba6afe1994877becaf7f026a3e93ecaf3af6
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-01-05 18:40:47 +0530

    new commit

--- DSA with STRIVERS/PATTERN QUSTIONS/1.js
+++ DSA with STRIVERS/PATTERN QUSTIONS/1.js
@@ -3,6 +3,18 @@
 // ****
 // ****
 
+function Rectengular(n){
+    let pattern = "";
+    for(let i=1;i<=n;i++){
+        for(let j =1;j<=n;j++){
+            pattern += "*";
+        }
+        pattern += "\n";
+    }
+    console.log(pattern)
+}
+Rectengular(5)
+
 
 // function square(x){
 //     var print ="";
