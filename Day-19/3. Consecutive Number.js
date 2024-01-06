5423907d1002dfc80e1ba4fac26e56d4d4b3d482
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-01-06 17:56:43 +0530

    new

--- Day-19/3. Consecutive Number.js
+++ Day-19/3. Consecutive Number.js
@@ -5,3 +5,33 @@
 
 // const inputB = [4, 2, 4, 3, 1];
 // Output = ("Not a consecutive since 4 is repeating");
+
+
+function Consecutive(arr){
+    let min = 1;
+    let max = 10;
+    let blank = [];
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]<min){
+            min = arr[i]
+        }
+        for(let j = i+1;j<arr.length;j++){
+            if(arr[i]!==arr[j]){
+                for(let p=min;p<=max;p++){
+                    if(!arr.includes(p)){
+                        blank.push(p);
+                    }
+                }
+            }
+            else{
+                console.log("Not a consecutive number is repeating")
+            }
+        }
+    }
+
+      
+
+    let finalblank = [...arr,...blank];
+        console.log(finalblank)
+}
+Consecutive([1, 3, 4, 5, 6])
