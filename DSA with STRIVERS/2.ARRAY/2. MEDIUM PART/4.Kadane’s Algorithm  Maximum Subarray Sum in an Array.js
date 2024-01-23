0e62b56ae1dc8248304d9bc00fc7615967c0933e
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-01-23 21:25:22 +0530

    23/01

--- DSA with STRIVERS/2.ARRAY/2. MEDIUM PART/4.Kadane’s Algorithm  Maximum Subarray Sum in an Array.js
+++ DSA with STRIVERS/2.ARRAY/2. MEDIUM PART/4.Kadane’s Algorithm  Maximum Subarray Sum in an Array.js
@@ -38,3 +38,10 @@ const arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4];
 const n = arr.length;
 const maxSum = maxSubarraySum(arr, n);
 console.log(`The maximum subarray sum is: ${maxSum}`);
+
+
+
+const arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4];
+const n = arr.length;
+const maxSum = maxSubarraySum(arr, n);
+console.log(`The maximum subarray sum is: ${maxSum}`);
