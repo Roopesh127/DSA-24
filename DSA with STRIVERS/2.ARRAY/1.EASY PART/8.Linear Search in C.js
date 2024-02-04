cfa13f59db797189ace58fad02aa0cac6680c5a1
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-04 21:22:24 +0530

    04/02

--- DSA with STRIVERS/2.ARRAY/1.EASY PART/8.Linear Search in C.js
+++ DSA with STRIVERS/2.ARRAY/1.EASY PART/8.Linear Search in C.js
@@ -1,14 +1,38 @@
+// Example 1:
+// Input: arr[]= 1 2 3 4 5, num = 3
+// Output: 2
+// Explanation: 3 is present in the 2nd index
+
+// Example 2:
+// Input: arr[]= 5 4 3 2 1, num = 5
+// Output: 0
+// Explanation: 5 is present in the 0th index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Linear Search in C
 // input --> arr = [1,2,3,4,5] , num = 3
 // output -> 2
-const arr = [1,2,3,4,5,6,7,8,9]
-const num = 1
-function searchIndex(){
- for(let i=0;i<arr.length;i++){
-    if( arr[i] == num){
-        return i;
-    }
-   }
-   return -1;
-}
-console.log(searchIndex(arr,num))
+
+// const arr = [1,2,3,4,5,6,7,8,9]
+// const num = 1
+// function searchIndex(){
+//  for(let i=0;i<arr.length;i++){
+//     if( arr[i] == num){
+//         return i;
+//     }
+//    }
+//    return -1;
+// }
+// console.log(searchIndex(arr,num))
--- DSA with STRIVERS/2.ARRAY/1.EASY PART/9.Union of Two Sorted Arrays.js
+++ DSA with STRIVERS/2.ARRAY/1.EASY PART/9.Union of Two Sorted Arrays.js
@@ -1,3 +1,42 @@
+// Example 1:
+// Input:
+// n = 5,m = 5.
+// arr1[] = {1,2,3,4,5}  
+// arr2[] = {2,3,4,4,5}
+// Output:
+//  {1,2,3,4,5}
+
+// Explanation: 
+// Common Elements in arr1 and arr2  are:  2,3,4,5
+// Distnict Elements in arr1 are : 1
+// Distnict Elemennts in arr2 are : No distinct elements.
+// Union of arr1 and arr2 is {1,2,3,4,5} 
+
+// Example 2:
+// Input:
+// n = 10,m = 7.
+// arr1[] = {1,2,3,4,5,6,7,8,9,10}
+// arr2[] = {2,3,4,4,5,11,12}
+// Output: {1,2,3,4,5,6,7,8,9,10,11,12}
+// Explanation: 
+// Common Elements in arr1 and arr2  are:  2,3,4,5
+// Distnict Elements in arr1 are : 1,6,7,8,9,10
+// Distnict Elemennts in arr2 are : 11,12
+// Union of arr1 and arr2 is {1,2,3,4,5,6,7,8,9,10,11,12} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Union of Two Sorted Arrays
 
 // Input:
@@ -7,21 +46,21 @@
 // Output:
 //  {1,2,3,4,5}
 
-arr1 = [1,2,3,4,5,6,7,11]
-arr2 = [3,5,4,6,9,11,34]
-function sortedUnion(){
-    let newsorted = [];
-    for(let i=0;i<arr1.length;i++){
-        if(!newsorted.includes(arr1[i])){
-            newsorted.push(arr1[i])
-        } 
-    }
+// arr1 = [1,2,3,4,5,6,7,11]
+// arr2 = [3,5,4,6,9,11,34]
+// function sortedUnion(){
+//     let newsorted = [];
+//     for(let i=0;i<arr1.length;i++){
+//         if(!newsorted.includes(arr1[i])){
+//             newsorted.push(arr1[i])
+//         } 
+//     }
 
-    for(let i=0;i<arr2.length;i++){
-        if(!newsorted.includes(arr2[i])){
-            newsorted.push(arr2[i])
-        }
-    }
-    return newsorted;
-} 
-console.log(sortedUnion(arr1,arr2))
+//     for(let i=0;i<arr2.length;i++){
+//         if(!newsorted.includes(arr2[i])){
+//             newsorted.push(arr2[i])
+//         }
+//     }
+//     return newsorted;
+// } 
+// console.log(sortedUnion(arr1,arr2))
