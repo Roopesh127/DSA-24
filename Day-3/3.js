72c93f3f8f523db8802bc1c45c71d1861b9f5352
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-03-11 12:16:37 +0530

    11/03

--- Day-3/3.js
+++ Day-3/3.js
@@ -16,16 +16,16 @@
 // console.log(longestword(" hello this is roopesh and how are you" ));
 
 function longestword(x){
-    for(let i of x)
+    for(let i of x){
         // console.log(i)
 
     let new1 = i.split("");
-        console.log(new1)
-    //     let max =0;
-    //     for(let z of new1){
-    //         max = z.length > max;
-    //         return max;
-    //     }
+        // console.log(new1)
+        let max =0;
+        for(let z of new1){
+            max = z.length > max;
+            return max;
+        }
     }
 }
 console.log(longestword(" hello this is roopesh and how are you" ));
