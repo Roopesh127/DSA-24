1a04bdd6740a38e24de1d5f74d4d9683d1daea27
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-04-01 00:00:02 +0530

    32

--- OTHER SPECIAL WORK AND QUESTION/ZENTECH INTERVIEW/5.Made a calculator.js
+++ OTHER SPECIAL WORK AND QUESTION/ZENTECH INTERVIEW/5.Made a calculator.js
@@ -1,27 +1,26 @@
 // // Made Calculator in javascript
 
-function Calculator(a,b){
-   function Addition(){
-    console.log(`Addition ${a+b}`);
-   }
-   Addition(a,b)
+function Calculator(a, b) {
+  function Addition() {
+    console.log(`Addition ${a + b}`);
+  }
+  Addition(a, b);
 
-   function Substraction(){
-    console.log(`Substraction ${a-b}`)
-   }
-   Substraction(a,b)
+  function Substraction() {
+    console.log(`Substraction ${a - b}`);
+  }
+  Substraction(a, b);
 
-   function Multiplication(){
-    console.log(`Multiplication ${a*b}`)
-   }
-   Multiplication(a,b);
+  function Multiplication() {
+    console.log(`Multiplication ${a * b}`);
+  }
+  Multiplication(a, b);
 
-   function Devidation(){
-    console.log(`Devidation ${a/b}`);
-   }
-   Devidation(a,b)
+  function Devidation() {
+    console.log(`Devidation ${a / b}`);
+  }
+  Devidation(a, b);
 }
-Calculator(5,2)
+Calculator(5, 2);
 ////////////////////
-
-jhjbjhgjgy v   
+ijfjjd;
