739687abf1c39ab3f5f6adbe16426df8bcb56022
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-04-05 23:16:04 +0530

    05/04

--- DSA with STRIVERS/2.ARRAY/1.EASY PART/14.Given an array and a sum k, we need to print the length of the longest subarray that sums to k.js
+++ DSA with STRIVERS/2.ARRAY/1.EASY PART/14.Given an array and a sum k, we need to print the length of the longest subarray that sums to k.js
@@ -5,29 +5,26 @@
 // Result: 2
 // Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
 
+// function getLongestSubarray(a, k) {
+//     let n = a.length; // size of the array
 
+//     let len = 0;
+//     for (let i = 0; i < n; i++) { // starting index
+//         for (let j = i; j < n; j++) { // ending index
+//             // add all the elements of subarray = a[i...j]
+//             let s = 0;
+//             for (let K = i; K <= j; K++) {
+//                 s += a[K];
+//             }
 
+//             if (s === k)
+//                 len = Math.max(len, j - i + 1);
+//         }
+//     }
+//     return len;
+// }
 
-function getLongestSubarray(a, k) {
-    let n = a.length; // size of the array
-
-    let len = 0;
-    for (let i = 0; i < n; i++) { // starting index
-        for (let j = i; j < n; j++) { // ending index
-            // add all the elements of subarray = a[i...j]
-            let s = 0;
-            for (let K = i; K <= j; K++) {
-                s += a[K];
-            }
-
-            if (s === k)
-                len = Math.max(len, j - i + 1);
-        }
-    }
-    return len;
-}
-
-let a = [-1, 1, 1];
-let k = 1;
-let len = getLongestSubarray(a, k);
-console.log("The length of the longest subarray is:", len);
+// let a = [-1, 1, 1];
+// let k = 1;
+// let len = getLongestSubarray(a, k);
+// console.log("The length of the longest subarray is:", len);
