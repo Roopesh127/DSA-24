eefff6851b481157dbcf65ff4b4780c3cb3dad04
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-04-18 22:50:33 +0530

    18/04

--- Day-8/Prime or not.js
+++ Day-8/Prime or not.js
@@ -1,17 +1,17 @@
 // To check if given number is prime or not
 
-// function CheckPrime(number){
-//       if(number<=1){
-//         return false;
-//       }
-//       for(let i=2;i<=Math.sqrt(number);i++){
-//         if(number%i == 0){
-//             return false;
-//         }
-//       }
-//       return true;
-// }
-// console.log(CheckPrime(	67280421310721));
+function CheckPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(CheckPrime(67280421310721));
 
 function checkPrime(n) {
   if (n < 2) {
