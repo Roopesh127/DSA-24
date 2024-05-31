65950cbeb02b88ffef5f12f8d9ef1d22bbecab10
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-05-31 22:37:00 +0530

    sum of two number by single loop

--- DSA with STRIVERS/2.ARRAY/1.EASY PART/14.Given an array and a sum k, we need to print the length of the longest subarray that sums to k.js
+++ DSA with STRIVERS/2.ARRAY/1.EASY PART/14.Given an array and a sum k, we need to print the length of the longest subarray that sums to k.js
@@ -1,4 +1,4 @@
-// Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
+ // Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
 
 // Example 1:
 // Input Format: N = 3, k = 5, array[] = {2,3,5}
