3c4aa3c7d1ee2444778042b8c145e13e54769ef5
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-06-03 19:10:03 +0530

    prepration for advantal

--- TOP -15 Question which is asked in every interview/1.Print n numbers.js
+++ TOP -15 Question which is asked in every interview/1.Print n numbers.js
@@ -1,9 +1,22 @@
 // print n numbers.
 
+// function Number(n){
+//   let print = [];
+//   for(let i=0;i<=n;i++){
+//      print.push(i)
+//   }
+//   return print;
+// }
+// console.log(Number(10))
+
+
+
+
+
 // by loop
 
-let n =15; 
-for(let i=0;i<=n;i++){
-    console.log(i)
-}
+// let n =15; 
+// for(let i=0;i<=n;i++){
+//     console.log(i)
+// }
 
